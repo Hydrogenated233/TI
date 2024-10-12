2088e68a24b04d5e644a17369b6d578931e94437
Hydrogenated233 <109909020+Hydrogenated233@users.noreply.github.com>
2024-10-12 17:39:16 +0800

    dev-2024-10-12
    1.添加三个升级
    2.添加新闻
    3.重构软上限储存
    4.修改“加强机器”的升级效果
    5.结局:2e6棵树+upg14

--- js/main.js
+++ js/main.js
@@ -3,9 +3,37 @@ var notify = document.getElementById('notify');
 let upgLookingAt=0;
 hideNotify();
 const f=30;
-let softs=[
-    N(5000)
+const softs=[
+    [N(5000), N(0.7), 'pow']
 ];
+function deepFreezeArray(arr) {  
+    // 首先冻结当前数组  
+    const frozenArr = Object.freeze(arr);  
+    // 遍历数组中的每个元素  
+    for (let i = 0; i < frozenArr.length; i++) {  
+      // 如果元素是数组，则递归地冻结它  
+      if (Array.isArray(frozenArr[i])) {  
+        deepFreezeArray(frozenArr[i]);  
+      }
+      // 如果元素是原始值（如数字、字符串、布尔值等），则不需要进一步处理  
+    } 
+}
+function deepCopyUnfrozenArray(arr) {  
+    // 创建一个新的空数组来存放副本  
+    let copy = [];  
+    // 遍历原数组的每个元素  
+    for (let i = 0; i < arr.length; i++) {  
+      const element = arr[i];
+      // 如果元素是数组，则递归地复制它  
+      if (Array.isArray(element)) {  
+        copy[i] = deepCopyUnfrozenArray(element);  
+      }else{   
+        copy[i] = element;  
+      }  
+    }  
+    return copy;  
+  } 
+deepFreezeArray(softs);
 
 let nowLookAt="page1";
 const upgradeEffect=[
@@ -20,6 +48,9 @@ const upgradeEffect=[
     [N(50),"木板"],
     [N(80),"木板"],
     [N(110),"木板"],
+    [N(300),"木板"],
+    [N(500),"木板"],
+    [N(1000),"木板"],
 ]
 
 /*
@@ -39,6 +70,7 @@ let player={
     money: N(0),
     upgrades: tmp,
     autoMoney: 0,
+    autoWood: 0,
     wood: N(0)
 };
 function getUndulatingColor(period = Math.sqrt(760)) {
@@ -52,7 +84,7 @@ function getUndulatingColor(period = Math.sqrt(760)) {
     return "#" + String(a) + String(b) + String(c)
 }
 function hardReset(){
-    var c1=prompt("您确定要硬重置吗？输入“八百标兵奔北坡”确定")=="八百标兵奔北坡";
+    var c1=prompt("您确定要硬重置吗？输入“111*111”的答案确定")=="12321";
     if(c1){
         let tmp=[];
         for(let i=0;i<upgradeEffect.length;i++){
@@ -65,6 +97,7 @@ function hardReset(){
             money: N(0),
             upgrades: tmp,
             autoMoney: 0,
+            autoWood: 0,
             wood: N(0)
         };
     }
@@ -129,10 +162,13 @@ function showText(id){
         "机器收割<br>*5<br><span id='upgrade5Cost'></span>",
         "合同<br>每秒获得10%$<br><span id='upgrade6Cost'></span>",
         "加强木<br>树价值*2<br><span id='upgrade7Cost'></span>",
-        "加强机器<br>*1+ln(木板)<br>*"+"<span id='up8Effect'>"+"</span>"+"<br><span id='upgrade8Cost'></span>",
+        "加强机器<br>*1+|ln(木板)|<br>*"+"<span id='up8Effect'>"+"</span>"+"<br><span id='upgrade8Cost'></span>",
         "肥料<br>软上限1 延迟500<br>"+"<span id='upgrade9Cost'></span>",
         "员工2<br>*6<br>"+"<span id='upgrade10Cost'></span>",
         "大树<br>树价值*2<br>"+"<span id='upgrade11Cost'></span>",
+        "金坷垃<br>软上限1 延迟1000<br>"+"<span id='upgrade12Cost'></span>",
+        "锯<br>每秒获得1%木板<br>"+"<span id='upgrade13Cost'></span>",
+        "更好的肥料<br>软上限1-> ^0.72<br>"+"<span id='upgrade14Cost'></span>",
         ]
         let text=document.getElementById("page"+JSON.stringify(id<7?1:2)+"Text");
         text.display="block";
@@ -156,30 +192,44 @@ function buy(id){
 }
 function upgEffect(){
   let gain=N(1);
-  softs=[
-    N(5000)
-];
-  player.treesMoney=N(1);
-  if(player.upgrades[0]) gain=gain.mul(1.5);
-  if(player.upgrades[1]) gain=gain.mul(3);
-  if(player.upgrades[2]) gain=gain.mul(5);
-  if(player.upgrades[3]) gain=gain.mul(10);
-  if(player.upgrades[4]) gain=gain.mul(5);
-  if(player.upgrades[5]) player.autoMoney=0.1;
-  if(player.upgrades[6]) player.treesMoney=player.treesMoney.mul(2);
-  if(player.upgrades[7]) gain=gain.mul(player.wood.ln().add(1));
-  if(player.upgrades[8])softs[0]=softs[0].add(500);
-  if(player.upgrades[9])gain=gain.mul(6);
-  if(player.upgrades[10])player.treesMoney=player.treesMoney.mul(2);
-  player.treesPerSec=gain;
-  gain=gain.softcap(softs[0],0.7,'pow')
-  for(let i=0;i<softs.length;i++) document.getElementById("soft"+(i+1)+"Start").innerHTML=softs[i];
-  return gain;
+    let softsCal=deepCopyUnfrozenArray(softs);
+    player.treesMoney=N(1);
+    if(player.upgrades[0]) gain=gain.mul(1.5);
+    if(player.upgrades[1]) gain=gain.mul(3);
+    if(player.upgrades[2]) gain=gain.mul(5);
+    if(player.upgrades[3]) gain=gain.mul(10);
+    if(player.upgrades[4]) gain=gain.mul(5);
+    if(player.upgrades[5]) player.autoMoney=0.1;
+    if(player.upgrades[6]) player.treesMoney=player.treesMoney.mul(2);
+    if(player.upgrades[7]) gain=gain.mul(player.wood.ln().abs().add(1));
+    if(player.upgrades[8]) softsCal[0][0]=softsCal[0][0].add(500);
+    if(player.upgrades[9]) gain=gain.mul(6);
+    if(player.upgrades[10]) player.treesMoney=player.treesMoney.mul(2);
+    if(player.upgrades[11]) softsCal[0][0]=softsCal[0][0].add(1000);
+    if(player.upgrades[12]) player.autoWood=0.01;
+    if(player.upgrades[13]) softsCal[0][1]=N(0.72);
+    player.treesPerSec=gain;
+    for(let i=0;i<softsCal.length;i++){
+        let e=document.getElementById("soft"+(i+1))
+        if(player.treesPerSec.gt(softsCal[i][0]))e.style.display="block";
+        else e.style.display="none";
+        gain=gain.softcap(softsCal[i][0],softsCal[i][1],softsCal[i][2]);
+        e=document.getElementById("soft"+(i+1)+"Start");
+        e.innerHTML=softsCal[i][0];
+        e=document.getElementById("soft"+(i+1)+"Effect");
+        e.innerHTML=softsCal[i][1];
+    }
+    for(let i=0;i<softsCal.length;i++){
+        
+    }
+    return gain;
 }
 function tick(){
     let moneyGain=player.trees.mul(player.treesMoney.mul(3));
+    let woodGain=player.trees.mul(N(0.0003).mul(player.treesMoney));
     player.trees=player.trees.add(upgEffect().div(f));
     player.money=player.money.add(moneyGain.mul(player.autoMoney).div(f));
+    player.wood=player.wood.add(woodGain.mul(player.autoWood).div(f));
     fixUpgradesColor();
     updateDisplay();
 }
@@ -199,11 +249,6 @@ function makeWood(){
 function updateDisplay(){
     let rains=document.getElementsByClassName('rain');
     for(let i=0;i<rains.length;i++)rains[i].style="color:"+getUndulatingColor();
-    for(let i=0;i<softs.length;i++){
-        let e=document.getElementById("soft"+(i+1))
-        if(player.treesPerSec.gt(softs[i]))e.style.display="block";
-        else e.style.display="none";
-    }
     setIdInnerHtml("treesDisplay",format(player.trees));
     setIdInnerHtml("treesPerSecDisplay",format(upgEffect()));
     setIdInnerHtml("treesPerSecDisplayNS",format(player.treesPerSec));
